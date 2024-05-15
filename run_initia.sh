9d98437075ce7d15a3d248a07e0265496f2b9b6d
zrhra.eth [Jettokōsutā] <122523974+zrhraJETTOKOSUTA@users.noreply.github.com>
2024-05-15 14:08:20 +0700

    Update run_initia.sh

--- run_initia.sh
+++ run_initia.sh
@@ -56,7 +56,7 @@ initiad init "$moniker" --chain-id initiation-1
 wget https://initia.s3.ap-southeast-1.amazonaws.com/initiation-1/genesis.json
 cp genesis.json ~/.initia/config/genesis.json
 
-ed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.initia/config/app.toml
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.initia/config/app.toml
 sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.initia/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.initia/config/app.toml
 
