2913929ed7354ca7fa9b458407121ca227bf336d
zrhra.eth [Jettokōsutā] <122523974+zrhraJETTOKOSUTA@users.noreply.github.com>
2024-05-15 11:51:08 +0700

    Update run_initia.sh

--- run_initia.sh
+++ run_initia.sh
@@ -4,7 +4,7 @@
 echo "Showing Airdrop Nobi logo..."
 curl -s https://raw.githubusercontent.com/zrhraJETTOKOSUTA/bash-nobi.sh/main/bash%20logo.sh | bash
 echo "Join the Airdrop Nobi Telegram channel: https://t.me/airdropnobi"
-sleep 2
+sleep 5
 
 # Updating and installing required packages
 echo "Updating and installing required packages..."
