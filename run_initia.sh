0a1f2c93823a37c6cfb1da3344928f575414bbcf
zrhra.eth [Jettokōsutā] <122523974+zrhraJETTOKOSUTA@users.noreply.github.com>
2024-05-15 13:34:42 +0700

    Update run_initia.sh

--- run_initia.sh
+++ run_initia.sh
@@ -9,7 +9,6 @@ echo "Updating and installing required packages..."
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y clang pkg-config libssl-dev curl git wget htop tmux build-essential jq make lz4 gcc unzip
 
-# install Go if needed
 if ! command -v go &> /dev/null || [[ $(go version | awk '{print $3}' | cut -d. -f2) -lt 19 ]]; then
     echo "Go version 1.19 or above is required. Installing the latest version..."
     cd $HOME
@@ -21,7 +20,6 @@ if ! command -v go &> /dev/null || [[ $(go version | awk '{print $3}' | cut -d. 
     source $HOME/.profile
 fi
 
-# Verify Go installation
 if ! command -v go &> /dev/null; then
     echo "Failed to install Go. Exiting..."
     exit 1
@@ -29,21 +27,18 @@ fi
 
 echo "Go version: $(go version)"
 
-# Verify git installation
 if ! command -v git &> /dev/null; then
     echo "Git is not installed. Installing..."
     sudo apt update
     sudo apt install -y git
 fi
 
-# Verify curl installation
 if ! command -v curl &> /dev/null; then
     echo "curl is not installed. Installing..."
     sudo apt update
     sudo apt install -y curl
 fi
 
-# Verify jq installation
 if ! command -v jq &> /dev/null; then
     echo "jq is not installed. Installing..."
     sudo apt update
@@ -65,6 +60,13 @@ cp genesis.json ~/.initia/config/genesis.json
 
 sed -i -e 's/external_address = \"\"/external_address = \"'$(curl httpbin.org/ip | jq -r .origin)':26656\"/g' ~/.initia/config/config.toml
 sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.15uinit,0.01uusdc\"|" ~/.initia/config/app.toml
+
+echo "Setting Peers"
+
+PEERS="e3ac92ce5b790c76ce07c5fa3b257d83a517f2f6@178.18.251.146:30656,2692225700832eb9b46c7b3fc6e4dea2ec044a78@34.126.156.141:26656,2a574706e4a1eba0e5e46733c232849778faf93b@84.247.137.184:53456,40d3f977d97d3c02bd5835070cc139f289e774da@168.119.10.134:26313,1f6633bc18eb06b6c0cab97d72c585a6d7a207bc@65.109.59.22:25756,4a988797d8d8473888640b76d7d238b86ce84a2c@23.158.24.168:26656,e3679e68616b2cd66908c460d0371ac3ed7795aa@176.34.17.102:26656,d2a8a00cd5c4431deb899bc39a057b8d8695be9e@138.201.37.195:53456,329227cf8632240914511faa9b43050a34aa863e@43.131.13.84:26656,517c8e70f2a20b8a3179a30fe6eb3ad80c407c07@37.60.231.212:26656,07632ab562028c3394ee8e78823069bfc8de7b4c@37.27.52.25:19656,028999a1696b45863ff84df12ebf2aebc5d40c2d@37.27.48.77:26656,3c44f7dbb473fee6d6e5471f22fa8d8095bd3969@185.219.142.137:53456,8db320e665dbe123af20c4a5c667a17dc146f4d0@51.75.144.149:26656,c424044f3249e73c050a7b45eb6561b52d0db456@158.220.124.183:53456,767fdcfdb0998209834b929c59a2b57d474cc496@207.148.114.112:26656,edcc2c7098c42ee348e50ac2242ff897f51405e9@65.109.34.205:36656,140c332230ac19f118e5882deaf00906a1dba467@185.219.142.119:53456,4eb031b59bd0210481390eefc656c916d47e7872@37.60.248.151:53456,ff9dbc6bb53227ef94dc75ab1ddcaeb2404e1b0b@178.170.47.171:26656,ffb9874da3e0ead65ad62ac2b569122f085c0774@149.28.134.228:26656" && \
+
+SEEDS="2eaa272622d1ba6796100ab39f58c75d458b9dbc@34.142.181.82:26656,c28827cb96c14c905b127b92065a3fb4cd77d7f6@testnet-seeds.whispernode.com:25756" && \
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.initia/config/config.toml
 
 curl -Ls https://ss-t.initia.nodestake.org/addrbook.json > ~/.initia/config/addrbook.json
 
@@ -90,4 +92,3 @@ sudo systemctl enable initiad
 sudo systemctl daemon-reload
 sudo systemctl restart initiad
 
-echo "Initia setup completed successfully."
